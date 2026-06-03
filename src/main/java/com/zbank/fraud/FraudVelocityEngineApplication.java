25199190cfe36306459fa7bacd2953e1e4346c36
Zain Ul Abidin <itzxain@outlook.com>
2026-06-04 00:04:29 +0500

    Main Spring Boot Entry Point
    To allow Eclipse to compile and run the entire application context, we need this main application class.

---
+++ src/main/java/com/zbank/fraud/FraudVelocityEngineApplication.java
@@ -0,0 +1,13 @@
+package com.zbank.fraud;
+
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudVelocityEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudVelocityEngineApplication.class, args);
+    }
+}
