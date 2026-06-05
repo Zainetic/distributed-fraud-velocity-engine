a0f8e4a0426258396ea9a53292268b6241474a05
Zain Ul Abidin <itzxain@outlook.com>
2026-06-05 13:25:17 +0500

    Update FraudAlertListener.java

--- src/main/java/com/zbank/fraud/service/FraudAlertListener.java
+++ src/main/java/com/zbank/fraud/service/FraudAlertListener.java
@@ -1,6 +1,8 @@
 package com.zbank.fraud.service;
 
+import com.zbank.fraud.entity.FraudAuditRecord;
 import com.zbank.fraud.event.FraudAlertEvent;
+import com.zbank.fraud.repository.FraudAuditRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,11 +12,31 @@ import org.springframework.stereotype.Service;
 public class FraudAlertListener {
 
     private static final Logger log = LoggerFactory.getLogger(FraudAlertListener.class);
+    
+    private final FraudAuditRepository auditRepository;
 
-    // This method listens to the topic. The moment an event drops in, this fires automatically.
+    public FraudAlertListener(FraudAuditRepository auditRepository) {
+        this.auditRepository = auditRepository;
+    }
+
     @KafkaListener(topics = "fraud-alerts", groupId = "zbank-fraud-group")
     public void consumeFraudAlert(FraudAlertEvent event) {
-        log.warn("🚨 ASYNC KAFKA ALERT RECEIVED: Account freeze initiated for Card {} due to: {}", 
+        
+        log.warn("🚨 ASYNC KAFKA ALERT RECEIVED: Processing decline for Card {} due to: {}", 
                  event.cardId(), event.declineReason());
+
+        // Map the event payload to our database entity
+        FraudAuditRecord auditLog = new FraudAuditRecord(
+                event.transactionId(),
+                event.cardId(),
+                event.attemptedAmount(),
+                event.declineReason(),
+                event.timestamp()
+        );
+
+        // Save it to PostgreSQL asynchronously
+        auditRepository.save(auditLog);
+        
+        log.info("💾 Successfully persisted audit log for transaction: {}", event.transactionId());
     }
 }
